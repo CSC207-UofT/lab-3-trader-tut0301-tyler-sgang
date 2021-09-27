6f1a1f169f7dd699ff922f081c0cc7eba5a99573
June Oh <JuneOh710@gmail.com>
2021-09-27 15:11:14 -0400

    Added donkey class but haven't tested

---
+++ src/main/java/Donkey.java
@@ -0,0 +1,38 @@
+public class Donkey implements  Drivable, Domesticatable, Tradable {
+    private int maxSpeed;
+
+    public Donkey() {
+        this.maxSpeed = 100;
+    }
+
+    @Override
+    public String sound() {
+        return "uhhh donkey?";
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        int maxSpeed = this.maxSpeed;
+        maxSpeed += maxSpeed + 1;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed--;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public int getPrice() {
+        return 1;
+    }
+
+    @Override
+    public String toString(){
+        return "are we there yet?";
+    }
+}
