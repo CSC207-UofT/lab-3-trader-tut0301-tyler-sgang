1f5d21a5b2f64fcbb1ab3168c83c6f2ec43f4d04
TylerCYan <tylercy.yan@mail.utoronto.ca>
2021-09-27 15:52:53 -0400

    Fixed error in Trader.java; and Added annotation @Override in FerrirariTest.java

--- src/main/java/Ferrari.java
+++ src/main/java/Ferrari.java
@@ -9,32 +9,32 @@ public class Ferrari implements Drivable, Domesticatable, Tradable{
         this.maxSpeed = 50;
     }
 
-
+    @Override
     public String sound() {
         return "ZOOM!";
     }
 
-
+    @Override
     public void upgradeSpeed() {
         this.maxSpeed += 5;
     }
 
-
+    @Override
     public void downgradeSpeed() {
         this.maxSpeed -= 5;
     }
 
-
+    @Override
     public int getMaxSpeed() {
         return this.maxSpeed;
     }
 
-
+    @Override
     public int getPrice() {
         return 25;
     }
 
-
+    @Override
     public String toString(){
         String id = super.toString();
         return id + " (Max speed: " + this.maxSpeed +")";
